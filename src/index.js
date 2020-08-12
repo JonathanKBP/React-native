71d2c382ceec7811f0692d69fec9b746601a4a5b
Jonathan Kevin <jonathan.kevinbp@hotmail.com>
2020-08-12 02:02:11 -0300

    Completo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+import api from './services/api.js';
 
 export default function App(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response.data);
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddProject(){
+    const response = await api.post('repositories', {
+      title: `Novo repositório ${Date.now()}`,
+      owner: 'Jonathan Kevin'
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+  }
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello World</Text>
-      </View>
+      
+      <SafeAreaView style={styles.container}> 
+        <FlatList
+          style={styles.container}
+          data={repositories}
+          keyExtrator={repository => repository.id}
+          renderItem={({ item: repository}) => (
+            <Text style={styles.repository} >{repository.title}</Text>
+        )}
+        />
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar repositório</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </>    
   );
 }
@@ -16,13 +54,25 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  title:{
+  
+  repository:{
     color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold'
+    fontSize: 30,
+  },
+  
+  button:{
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText:{
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 
 });
